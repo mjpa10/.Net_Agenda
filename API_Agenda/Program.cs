61fdf40f0e8da27d3c90fa27279103b23ed33c9a
Matheus José Pereira de Andrade <matheus.andrade@portodorecife.pe.gov.br>
2024-08-30 12:08:33 -0300

    Adicionado serviço do contexto na classe program :check:

--- API_Agenda/Program.cs
+++ API_Agenda/Program.cs
@@ -1,11 +1,19 @@
+using APIAgenda.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection,
+        ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
