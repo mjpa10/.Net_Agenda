8e2cba0c6d0dad828d02586ce7265ada7d0208de
Matheus Jose <m.theus.jose.pereira@gmail.com>
2024-08-29 22:20:00 -0300

    Criando o Contexto do modelo

---
+++ API_Agenda/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace APIAgenda.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Contato>? Contatos { get; set; }
+}
