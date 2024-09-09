cc8e75b37e7c2532f734a01d0586ea2ea74c006d
Matheus Jose <m.theus.jose.pereira@gmail.com>
2024-09-09 09:14:18 -0300

    Adicionado conexção MySql

--- API_Agenda/Program.cs
+++ API_Agenda/Program.cs
@@ -1,3 +1,6 @@
+using APIAgenda.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string? mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection
+    ,ServerVersion.AutoDetect(mySqlConnection)));   
 
 var app = builder.Build();
 
