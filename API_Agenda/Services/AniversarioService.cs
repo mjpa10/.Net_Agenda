34cea7e5f57764d96c7b655cb3b7f32acf84b811
Matheus Jose <m.theus.jose.pereira@gmail.com>
2024-09-10 10:29:51 -0300

    ajustando Responsabilidades

---
+++ API_Agenda/Services/AniversarioService.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Agenda.Services
+{
+    public class AniversarioService
+    {
+        //Pode conter a lógica de negócios para lidar com aniversários,
+        //como cálculos de datas especiais ou promoções relacionadas.
+    }
+}
---
+++ API_Agenda/Services/EmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Agenda.Services
+{
+    public class EmailService
+    {
+        //Pode lidar com operações relacionadas ao envio de e-mails, se o projeto tiver essa funcionalidade.
+        //por exemplo, mandar emails quando o aniversario de alguem se aproxima
+    }
+}
